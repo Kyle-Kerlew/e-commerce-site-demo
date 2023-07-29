3e2f95676297f59444131f27cc33c37db258805d
Kyle Kerlew <kylekerlew1@gmail.com>
2023-07-29 18:36:49 -0400

    Adjust landing page design

--- src/components/shared/checkout/checkoutProgress.js
+++ src/components/shared/checkout/checkoutProgress.js
@@ -3,13 +3,13 @@ import CheckoutState from "../../../types/checkoutState";
 import "./checkoutProgress.css";
 import Arrow from "../../../images/arrow.svg";
 
-function CheckoutProgress({stage}) {
+function CheckoutProgress({ stage }) {
     return (
         <div className="is-flex is-justify-content-center checkout-progress">
             <span className={stage === CheckoutState.CART && "is-underlined"}>Cart</span>
-            <img className="no-gap" src={Arrow} alt="Arrow"/>
+            <img className="no-gap" src={Arrow} alt="Arrow" />
             <span className={stage === CheckoutState.ADDRESS && "is-underlined"}>Shipping</span>
-            <img className="no-gap" src={Arrow} alt="Arrow"/>
+            <img className="no-gap" src={Arrow} alt="Arrow" />
             <span className={stage === CheckoutState.PAYMENT && "is-underlined"}>Payment</span>
         </div>
     )
