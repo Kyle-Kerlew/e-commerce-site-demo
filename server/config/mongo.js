c94299d0acb1118698649fa3ec689de0e8ea7488
Kyle Kerlew <kylekerlew1@gmail.com>
2023-10-13 18:37:50 -0400

    add env mongodb uri

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,7 +1,7 @@
 import { MongoClient, ObjectId } from "mongodb";
 
 //TODO: REMOVE URL FROM GIT HISTORY AND ADD TO ENV FILE
-const uri = "";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 const db = client.db('infusion_creations')
 
